24f96e7206f87afd2fb8e1627c328599322ea4e8
Arnau Gallofré <masokismguys@gmail.com>
2018-12-26 13:13:52 +0100

    adjusment walls and camera

--- BulletGame/ModuleSceneIntro.cpp
+++ BulletGame/ModuleSceneIntro.cpp
@@ -18,13 +18,13 @@ bool ModuleSceneIntro::Start()
 	LOG("Loading Intro assets");
 	bool ret = true;
 
-	App->camera->Move(vec3(95,95,-50));
-	App->camera->LookAt(vec3(15, -8, 57));
+	App->camera->Move(vec3(95,145,-50));
+	App->camera->LookAt(vec3(15, -58, 57));
 	
 	
 	//Goal
 	goal.radius = 5.0f;
-	goal.SetPos(35, 3 , 138);
+	goal.SetPos(35, 3 , 100);
 	goal.color = Blue;
 	pb_goal = App->physics->AddBody(goal, 1.0f);
 
@@ -50,52 +50,52 @@ bool ModuleSceneIntro::Start()
 	App->physics->AddConstraintHinge(*pb_door2, *pb_pivot, vec3(9, 0, 0), vec3(0, 0, 0), vec3(0, 1, 0), vec3(0, 1, 0));
 
 	//Map Walls
-	wall1.size = { 30, 4, 4 };
-	wall1.SetPos(0, 2, 0);
+	wall1.size = { 30, 8, 4 };
+	wall1.SetPos(0, 4, 0);
 	pb_wall1 = App->physics->AddBody(wall1, 0.0f);
 
-	wall2.size = { 4, 4, 60 };
-	wall2.SetPos(17, 2, 28);
+	wall2.size = { 4, 8, 60 };
+	wall2.SetPos(17, 4, 28);
 	pb_wall2 = App->physics->AddBody(wall2, 0.0f);
 
-	wall3.size = { 4,4,100 };
-	wall3.SetPos(-17, 2, 48);
+	wall3.size = { 4,8,100 };
+	wall3.SetPos(-17, 4, 48);
 	pb_wall3 = App->physics->AddBody(wall3, 0.0f);
 
-	wall4.size = { 30,4,4 };
-	wall4.SetPos(0, 2, 96);
+	wall4.size = { 30,8,4 };
+	wall4.SetPos(0, 4, 96);
 	pb_wall4 = App->physics->AddBody(wall4, 0.0f);
 	
-	wall5.size = { 40,4,4 };
-	wall5.SetPos(35, 2, 60);
+	wall5.size = { 40,8,4 };
+	wall5.SetPos(35, 4, 60);
 	pb_wall5 = App->physics->AddBody(wall5, 0.0f);
 	
-	wall6.size = { 4,4,80 };
-	wall6.SetPos(57, 2, 98);
+	wall6.size = { 4,8,80 };
+	wall6.SetPos(57, 4, 98);
 	pb_wall6 = App->physics->AddBody(wall6, 0.0f);
 	
-	wall7.size = { 4,4,40 };
-	wall7.SetPos(17, 2, 114);
+	wall7.size = { 4,8,40 };
+	wall7.SetPos(17, 4, 114);
 	pb_wall7 = App->physics->AddBody(wall7, 0.0f);
 	
-	wall8.size = { 12,4,4 };
-	wall8.SetPos(49, 2, 136);
+	wall8.size = { 12,8,4 };
+	wall8.SetPos(49, 4, 136);
 	pb_wall8 = App->physics->AddBody(wall8, 0.0f);
 
-	wall9.size = { 12,4,4 };
-	wall9.SetPos(21, 2, 136);
+	wall9.size = { 12,8,4 };
+	wall9.SetPos(21, 4, 136);
 	pb_wall9 = App->physics->AddBody(wall9, 0.0f);
 
-	wall10.size = { 4,4,8 };
-	wall10.SetPos(25,2,142);
+	wall10.size = { 4,8,8 };
+	wall10.SetPos(25,4,142);
 	pb_wall10 = App->physics->AddBody(wall10, 0.0f);
 
-	wall11.size = { 4,4,8 };
-	wall11.SetPos(45, 2, 142);
+	wall11.size = { 4,8,8 };
+	wall11.SetPos(45, 4, 142);
 	pb_wall11 = App->physics->AddBody(wall11, 0.0f);
 
-	wall12.size = { 24,4,4 };
-	wall12.SetPos(35, 2, 148);
+	wall12.size = { 24,8,4 };
+	wall12.SetPos(35, 4, 148);
 	pb_wall12 = App->physics->AddBody(wall12, 0.0f);
 
 
